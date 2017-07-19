0fb6682f61ec65d5cc275b72200f4767ad3c8722
offertoro <gilad@offertoro.com>
2017-07-19 19:45:23 +0300

    Update crons.php

--- server/crons.php
+++ server/crons.php
@@ -1,6 +1,11 @@
 <?php
 
-class crons{
-  
+// VideoViewer cron jobs
+
+// this file is supposed to run internally and not be exposed
+
+class VideoViewerCrons {
+    // fill with logic that is supposed to be run externally by a cron job
 
 }
+
